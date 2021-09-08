f2d9fb5da646f3327162b2d09f7f7508a5eac5b9
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-09-08 16:12:55 +0200

    Added the navBar and styled it.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import CategoryBox from './CategoryBox'
-
+import hero from '../assets/hero.png'
 
 export default function Home() {
     return (
-        <div>
+        <div className='hero'>
+            <img src={hero} />
             <p>home</p>
             <CategoryBox />
         </div>
