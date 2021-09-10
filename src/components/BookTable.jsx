e65f0796906e984ba1725235bc77e46623107912
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-09-10 19:45:21 +0200

    Added contact us page and styled it.

---
+++ src/components/BookTable.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function BookTable() {
+  
+  return (
+    <div>
+      <form className='bookTable'>
+        <label>Full Name</label>
+        <input type="text" />
+        <label>Email</label>
+        <input type="email" />
+        <label>Date</label>
+        <input type='date' />
+        Time
+        <input type='time' />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
